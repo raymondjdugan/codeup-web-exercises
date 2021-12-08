e53b0260d3df2f7838595e551450515228cb4882
Raymond Dugan <raymond.j.dugan@gmail.com>
2021-12-08 12:01:30 -0600

    adding hello-asteroid file

---
+++ js/hello-asteroid.js
@@ -0,0 +1,7 @@
+// console.log('My favorite food is nacho\'s. ' + 'The gooey cheese is the best part. ' + 'I also really enjoy some
+// type of meat and lots of jalapeno\'s!');
+
+var favoriteFood = 'My favorite food is nacho\'s';
+var favoriteFoodAddon = 'jalapeno\'s';
+
+console.log(favoriteFood + ' with ' + favoriteFoodAddon + '!');
