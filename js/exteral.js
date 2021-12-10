e42a490e70ee6b8972028d67faa45c4276183d9f
Raymond Dugan <raymond.j.dugan@gmail.com>
2021-12-10 13:23:52 -0600

    uploading new files after js in html lesson

---
+++ js/exteral.js
@@ -0,0 +1,6 @@
+'use strict'
+console.log('Hello from external Javascript');
+
+alert('Welcome to my Website');
+let favColor = prompt('What is your favorite color?');
+alert(`Great, ${favColor} is my favorite color too!`);
