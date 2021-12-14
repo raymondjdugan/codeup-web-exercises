ac95a2f63a8bc2224bee588646847612bb46f840
Raymond Dugan <raymond.j.dugan@gmail.com>
2021-12-14 09:21:48 -0600

    worked on warmup exercise

--- js/warmup.js
+++ js/warmup.js
@@ -16,10 +16,19 @@
 
     // 12/13/21
     //Declare a function called 'isFive' that takes in an input and returns true if that input is the number five. Otherwise it should return false.
-    let random = Math.floor((Math.random() * 5) + 1);
-    console.log(random);
-    function isFive(number) {
-        return number === 5;
+    // let random = Math.floor((Math.random() * 5) + 1);
+    // console.log(random);
+    // function isFive(number) {
+    //     return number === 5;
+    // }
+    // console.log(isFive(random));
+
+    //Make a function named isMultipleOfThree that takes in an input and returns a boolean indicating whether or not
+    // the input is a multiple of three.
+    function isMultipleOfThree(someNumber){
+        return someNumber % 3 === 0 ? true : false;
     }
-    console.log(isFive(random));
+
+    console.log(isMultipleOfThree(15));
+    console.log(isMultipleOfThree(10));
 })();
