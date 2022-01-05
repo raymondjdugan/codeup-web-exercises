39d3596ef46654a0752af901c143495b6276be36
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-01-05 08:25:17 -0600

    completed circle.js exercises

--- js/circle.js
+++ js/circle.js
@@ -2,14 +2,15 @@
     "use strict";
 
     // create a circle object
-    var circle = {
+    let circle = {
         radius: 3,
 
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
+            let area = Math.PI * Math.pow(this.radius, 2);
+            return area; // TODO: return the proper value
 
-            return; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
@@ -17,8 +18,7 @@
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log(`Area of a circle with radius: ${this.radius} is: ${doRounding === true ? Math.round(this.getArea()) : this.getArea()}`);
         }
     };
 
@@ -30,7 +30,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
