64b22840765a6145492c72c48d899fc649952222
RayDugan <raymond.j.dugan@gmail.com>
2022-01-14 15:53:36 -0600

    refactored code. Started bootstrap

--- js/Non-Graded JS/warmup3.js
+++ js/Non-Graded JS/warmup3.js
@@ -1,20 +1,36 @@
 (function () {
-    // TODO: Create a function called arrayToString.
-    //       The function takes a parameter called myArray.
-    //       It returns a string that is the concatenation of all the elements in myArray.
-    //       This function is essentially your own version of join.
-    //       Therefore, you cannot use join in your function body :slightly_smiling_face:
+    // // TODO: Create a function called arrayToString.
+    // //       The function takes a parameter called myArray.
+    // //       It returns a string that is the concatenation of all the elements in myArray.
+    // //       This function is essentially your own version of join.
+    // //       Therefore, you cannot use join in your function body :slightly_smiling_face:
+    //
+    //
+    // const myJoin = function (myArray, delim) {
+    //     let str = '';
+    //     for (let i = 0; i < myArray.length; i++) {
+    //         str += `${myArray[i]}${delim}`;
+    //     }
+    //     return str.slice(0, str.length - 1);
+    // }
+    //
+    // console.log(myJoin(['Ray', 'Chelsea', 'Clara'], ","));
 
 
-    const myJoin = function (myArray) {
-        let str = '';
-        for (let i = 0; i < myArray.length; i++) {
-            str += `${myArray[i]}, `;
-        }
-        return str.slice(0, str.length - 2);
+    let bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+    // const removeAll = function(myArray, valueToRemove) {
+    //     // return myArray.filter(el => valueToRemove !== el);
+    //     return myArray.filter(function (el) {
+    //         return valueToRemove !== el;
+    //     })
+    // }
+    // console.log(removeAll(bugs, 'ant'));
+    const removeAll = function (array, value){
+        const arraySet = new Set(array);
+        console.log(arraySet)
+        arraySet.delete(value);
+        return Array.from(arraySet);
     }
-
-    console.log(myJoin(['Ray', 'Chelsea', 'Clara']));
-
+    console.log(removeAll(bugs, 'ant'));
 })();
 
