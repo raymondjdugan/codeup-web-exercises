cbc2554f2e41bec0bcc4304940084ecc22d071d2
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-02-17 16:02:20 -0600

    finished custom promise

--- js/promises.js
+++ js/promises.js
@@ -1,11 +1,40 @@
 // fetch('data/inventory.json').then(r => console.log(r))
+const formatDate = function (date) {
+    console.log(new Date(date).toLocaleDateString())
+}
 
-function getGithubUsernames(username) {
-    return fetch(`https://api.github.com/users/${username}/events`, {headers: { Authorization: `${GITHUB_KEY}`}})
-        .then(response => response.json())
-        .then(result => {
-            console.log(result)
+const getLastCommit = function (repoName){
+    let [username, repo] = repoName.split("/")
+    return fetch(`https://api.github.com/repos/${username}/${repo}/commits`, {headers: { Authorization: `${GITHUB_KEY}`}})
+        .then(response => {
+            return response.json()
+        }).then(result => {
+            formatDate(result[0].commit.author.date)
         }).catch(error => alert('No Commits'))
 }
 
-getGithubUsernames('raymondjdugan');
+const getLastRepo = function (username) {
+    return fetch(`https://api.github.com/users/${username}/events`, {headers: { Authorization: `token ${GITHUB_KEY}`}})
+        .then(response => {
+            return response.json()
+        })
+        .then(result => {
+            // console.log(result)
+            // console.log(result[0].repo.name)
+            getLastCommit(result[0].repo.name)
+        }).catch(error => alert('No Commits'))
+}
+
+getLastRepo('raymondjdugan');
+
+// Custom promise
+
+function wait(time) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+                resolve(time)
+        }, time);
+    });
+}
+wait(1000).then(r => console.log(`You'll see this after ${r} seconds`));
+wait(3000).then(r => console.log(`You'll see this after ${r} seconds`));
