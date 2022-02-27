483bfee072021ba7b9ac8d92d382ddbf3a13750f
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-02-26 21:53:49 -0600

    testing mapbox api elements and functionality

---
+++ js/Non-Graded JS/hello.js
@@ -0,0 +1,19 @@
+const map = function (){
+    mapboxgl.accessToken = RAYMOND_DUGAN_KEY;
+    return new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        zoom: 10,
+        center: [-101.8313, 35.2220],
+        pitch: 50, // pitch in degrees
+    });
+}
+console.log(map())
+const geocoder = function() {
+    return new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    });
+}
+console.log(geocoder())
+$('#geocoder').append(geocoder().addTo(map));
